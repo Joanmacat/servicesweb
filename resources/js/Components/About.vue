65c235947d562810770261e2ce43fa41b27109ef
MarcJuanmarti <joanmacat@gmail.com>
2023-12-19 10:57:15 +0100

    Adds about page

--- resources/js/Components/About.vue
+++ resources/js/Components/About.vue
@@ -8,7 +8,7 @@
         <h2
           class="text-2xl font-bold md:text-4xl md:leading-tight dark:text-white"
         >
-          Your questions, answered
+          About Andorra Services
         </h2>
         <p class="mt-1 text-gray-600 dark:text-gray-400">
           Answers to the most frequently asked questions.
@@ -27,7 +27,7 @@
               class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400"
               aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-one"
             >
-              Can I cancel at anytime?
+              Who we are and what we do?
               <svg
                 class="hs-accordion-active:hidden block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
                 width="16"
@@ -65,9 +65,8 @@
               aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-one"
             >
               <p class="text-gray-800 dark:text-gray-200">
-                Yes, you can cancel anytime no questions are asked while you
-                cancel but we would highly appreciate if you will give us some
-                feedback.
+                We are a platform dedicated to providing a comprehensive list of services and companies in Andorra, making it easier for users to find and connect with them. Our goal is to facilitate seamless communication between companies and potential customers. 
+                We understand the importance of keeping information up-to-date, and we take this responsibility seriously.
               </p>
             </div>
           </div>
@@ -80,7 +79,7 @@
               class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400"
               aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-two"
             >
-              My team has credits. How do we use them?
+              Who we aren't?
               <svg
                 class="hs-accordion-active:hidden block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
                 width="16"
@@ -118,8 +117,7 @@
               aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-two"
             >
               <p class="text-gray-800 dark:text-gray-200">
-                Once your team signs up for a subscription plan. This is where we
-                sit down, grab a cup of coffee and dial in the details.
+                We would like to clarify that Serveisweb.ad is not affiliated with any government entity, nor have we received any funding or financial support from public entities. 
               </p>
             </div>
           </div>
@@ -132,7 +130,7 @@
               class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400"
               aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-three"
             >
-              How does Preline's pricing work?
+              What we don't do?
               <svg
                 class="hs-accordion-active:hidden block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
                 width="16"
@@ -170,8 +168,8 @@
               aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-three"
             >
               <p class="text-gray-800 dark:text-gray-200">
-                Our subscriptions are tiered. Understanding the task at hand and
-                ironing out the wrinkles is key.
+                We prioritize the privacy and security of our users. We want to assure you that we do not store any sensitive information from any customer. We understand the importance of safeguarding personal data and adhere to strict data protection practices. 
+                For detailed information regarding our data collection, storage, and usage policies, we encourage you to refer to our Terms and Conditions. 
               </p>
             </div>
           </div>
@@ -184,7 +182,7 @@
               class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400"
               aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-four"
             >
-              How secure is Preline?
+              About advertisement
               <svg
                 class="hs-accordion-active:hidden block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
                 width="16"
@@ -222,9 +220,7 @@
               aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-four"
             >
               <p class="text-gray-800 dark:text-gray-200">
-                Protecting the data you trust to Preline is our first priority.
-                This part is really crucial in keeping the project in line to
-                completion.
+                Our advertising is solely conducted through Google Ads. We do not engage in selling any services to improve the position or ranking of any service listed on our website. Our aim is to provide unbiased and accurate information to our users, without any bias towards specific companies or services. 
               </p>
             </div>
           </div>
@@ -237,7 +233,7 @@
               class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400"
               aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-five"
             >
-              How do I get access to a theme I purchased?
+              Contact us!
               <svg
                 class="hs-accordion-active:hidden block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
                 width="16"
@@ -275,66 +271,7 @@
               aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-five"
             >
               <p class="text-gray-800 dark:text-gray-200">
-                If you lose the link for a theme you purchased, don't panic! We've
-                got you covered. You can login to your account, tap your avatar in
-                the upper right corner, and tap Purchases. If you didn't create a
-                login or can't remember the information, you can use our handy
-                Redownload page, just remember to use the same email you
-                originally made your purchases with.
-              </p>
-            </div>
-          </div>
-  
-          <div
-            class="hs-accordion hs-accordion-active:bg-gray-100 rounded-xl p-6 dark:hs-accordion-active:bg-white/[.05]"
-            id="hs-basic-with-title-and-arrow-stretched-heading-six"
-          >
-            <button
-              class="hs-accordion-toggle group pb-3 inline-flex items-center justify-between gap-x-3 w-full md:text-lg font-semibold text-left text-gray-800 transition hover:text-gray-500 dark:text-gray-200 dark:hover:text-gray-400"
-              aria-controls="hs-basic-with-title-and-arrow-stretched-collapse-six"
-            >
-              Upgrade License Type
-              <svg
-                class="hs-accordion-active:hidden block w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M2 5L8.16086 10.6869C8.35239 10.8637 8.64761 10.8637 8.83914 10.6869L15 5"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                />
-              </svg>
-              <svg
-                class="hs-accordion-active:block hidden w-3 h-3 text-gray-600 group-hover:text-gray-500 dark:text-gray-400"
-                width="16"
-                height="16"
-                viewBox="0 0 16 16"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M2 11L8.16086 5.31305C8.35239 5.13625 8.64761 5.13625 8.83914 5.31305L15 11"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                />
-              </svg>
-            </button>
-            <div
-              id="hs-basic-with-title-and-arrow-stretched-collapse-six"
-              class="hs-accordion-content hidden w-full overflow-hidden transition-[height] duration-300"
-              aria-labelledby="hs-basic-with-title-and-arrow-stretched-heading-six"
-            >
-              <p class="text-gray-800 dark:text-gray-200">
-                There may be times when you need to upgrade your license from the
-                original type you purchased and we have a solution that ensures
-                you can apply your original purchase cost to the new license
-                purchase.
+                If you have any questions or inquiries, we encourage you to reach out to us through our Contact page. 
               </p>
             </div>
           </div>
