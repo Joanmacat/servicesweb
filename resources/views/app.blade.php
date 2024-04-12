c0bac81a589fdecc7b931e4b805431bdc198199d
Marc Juanmartí <marc@nectar.tech>
2024-04-12 11:59:43 +0200

    Deployment commit

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Andorra Connect - Your business gateway</title>
+        <title>Andorra Connect - Explore services for business in Andorra.</title>
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
