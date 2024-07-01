b9d4048aee1d865a2f15af26863dc741e7191ba5
Marc Juanmartí <marc@nectar.tech>
2024-07-01 16:02:42 +0200

    Add new article women

---
+++ resources/js/Pages/Articles/Women-rights-in-andorra.vue
@@ -0,0 +1,100 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import HomeNavbar from '../../Components/HomeNavbar.vue';
+import Footer from '../../Components/Footer.vue';
+</script>
+
+<template>
+<Head title="The EU Agreement Should “Facilitate the Internationalization” of Andorran Companies." />
+<HomeNavbar></HomeNavbar>
+<!-- Blog Article -->
+<div class="max-w-2xl px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto">
+  <div class="max-w-2xl place-content-center">
+    <!-- Avatar Media -->
+    <div class="flex justify-between items-center mb-6">
+      <div class="flex w-full sm:items-center gap-x-5 sm:gap-x-3">
+        <div class="flex-shrink-0 p-6">
+          <img class="w-7 rounded-full" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Anna Ludic">
+        </div>
+
+        <div class="grow">
+          <div class="flex justify-between items-center gap-x-2">
+            <div>
+              <!-- Tooltip -->
+              <div class="hs-tooltip [--trigger:hover] [--placement:bottom] inline-block">
+                <div class="hs-tooltip-toggle sm:mb-1 block text-start cursor-pointer">
+                  <span class="font-semibold text-gray-800 dark:text-neutral-200">
+                    Anna Ludic
+                  </span>
+
+                  <!-- Dropdown Card -->
+                  <div class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 max-w-xs cursor-default bg-gray-900 divide-y divide-gray-700 shadow-lg rounded-xl dark:bg-neutral-950 dark:divide-neutral-700" role="tooltip">
+                  </div>
+                  <!-- End Dropdown Card -->
+                </div>
+              </div>
+              <!-- End Tooltip -->
+
+              <ul class="text-xs text-gray-500 dark:text-neutral-500">
+                <li class="inline-block relative pe-6 last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-2 before:-translate-y-1/2 before:size-1 before:bg-gray-300 before:rounded-full dark:text-neutral-400 dark:before:bg-neutral-600">
+                  20/06/2024
+                </li>
+                <li class="inline-block relative pe-6 last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-2 before:-translate-y-1/2 before:size-1 before:bg-gray-300 before:rounded-full dark:text-neutral-400 dark:before:bg-neutral-600">
+                  3 min read
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Avatar Media -->
+
+    <!-- Content -->
+    <div class="space-y-5 md:space-y-8">
+      <div class="space-y-3">
+        <h2 class="text-2xl font-bold md:text-3xl dark:text-white">Advancing Women’s Rights in Andorra</h2>
+
+        <p class="text-lg text-gray-800 dark:text-neutral-200">Andorra, nestled between France and Spain, is a small European country that has made significant strides in ensuring equal opportunities and protections for women under the law. Central to this effort are various pieces of legislation aimed at combating discrimination, preventing violence, and promoting gender parity across different spheres of society.</p>
+      </div>
+
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">In 2022, the country took a significant step toward gender equality by enacting a law that ensures equal treatment and opportunities for both women and men, thereby prohibiting gender-based discrimination. This landmark legislation outlines measures to foster gender parity in employment, education, and public life. It underscores Andorra’s commitment to valuing individuals based on their skills and contributions, regardless of gender.</p>
+      
+      <figure class="mt-6 mb-6">
+        <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1520785145672-94e9abd8b121?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="The EU Agreement Should “Facilitate the Internationalization” of Andorran Companies.">
+        <figcaption class="mt-3 text-sm text-center text-gray-500 dark:text-neutral-500">
+          This landmark legislation outlines measures to foster gender parity in employment, education, and public life.
+        </figcaption>
+      </figure>
+
+      <h3 class="mt-6 text-2xl font-semibold dark:text-white">Combating Domestic Violence and Sexual Harassment</h3>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">To address domestic violence, Andorra has implemented robust legal measures to protect victims and hold perpetrators accountable. The Law on Domestic Violence Prevention and Protection provides a legal framework for addressing domestic violence and ensuring the safety and well-being of survivors.</p>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">Furthermore, Andorra’s laws clearly outline how to report incidents, obtain protection orders, and access support services, demonstrating the country’s firm stance against domestic violence. Andorra also combats sexual harassment through legislation, ensuring that victims have avenues for action and remedy. This approach reflects Andorra’s commitment to creating a respectful, intimidation-free environment where survivors receive the necessary support to pursue justice and rebuild their lives.</p>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">In addition, Andorra has enacted laws against sexual harassment, covering workplaces and other environments. These laws define sexual harassment, outline reporting processes, and aim to foster a culture of respect, ensuring everyone’s right to work and live free from harassment and fear.</p>
+      
+      <figure class="mt-6 mb-6">
+        <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1633354577213-e64866b8a778?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="The EU Agreement Should “Facilitate the Internationalization” of Andorran Companies.">
+        <figcaption class="mt-3 text-sm text-center text-gray-500 dark:text-neutral-500">
+          To address domestic violence, Andorra has implemented robust legal measures to protect victims and hold perpetrators accountable.
+        </figcaption>
+      </figure>
+
+      <p class="text-lg text-gray-800 dark:text-neutral-200">In 2021, Andorra made strides in political gender equality by implementing legal amendments aligned with recommendations from the CEDAW Committee. These changes mandate a gender quota, requiring that each electoral list includes at least 40% of candidates from each gender, thus promoting balanced representation in democratic institutions.</p>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">Moreover, the Law on Political Parties mandates that political parties actively promote gender equality within their organizations. This may include measures such as ensuring gender balance in party leadership positions and candidate lists for elections. Andorra promotes women’s voices and perspectives in its decision-making processes by advocating for increased political presence.</p>
+
+      <div class="space-y-3">
+        <h3 class="mt-6 text-2xl font-semibold dark:text-white">Promoting Gender Equality in Politics and the Workplace</h3>
+
+        <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">Furthermore, labor laws in Andorra play a crucial role in advancing women’s rights and gender equality. These laws prohibit discrimination in employment based on gender and ensure equal pay for equal work. By preventing employers from discriminating against women in hiring, promotion, or terms and conditions of employment, the nation aims to create a level playing field where individuals are evaluated based on their skills and qualifications rather than their gender.</p>
+        <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">Andorra’s commitment to progressive laws and initiatives underscores its dedication to building an equitable society where everyone, regardless of gender, can achieve their highest potential.</p>
+        
+        <a href="https://borgenproject.org/womens-rights-in-andorra/" target="_blank"><p class="mt-6 text-lg text-gray-800 dark:text-neutral-200 underline decoration-2">Check the original article in here</p></a>
+
+      </div>
+    </div>
+    <!-- End Content -->
+  </div>
+</div>
+<!-- End Blog Article -->
+<Footer></Footer>
+</template>
