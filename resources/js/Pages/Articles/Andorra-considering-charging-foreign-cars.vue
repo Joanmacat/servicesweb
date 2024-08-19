25c77063e686ec0f9b607339511c52a27d69af49
Marc Juanmartí <marc@nectar.tech>
2024-08-19 12:35:30 +0200

    Update axios and adds new article

---
+++ resources/js/Pages/Articles/Andorra-considering-charging-foreign-cars.vue
@@ -0,0 +1,99 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import HomeNavbar from '../../Components/HomeNavbar.vue';
+import Footer from '../../Components/Footer.vue';
+
+document.title = "Andorra is considering charging foreign cars that wish to enter the country - Andorra Connect"
+</script>
+
+<template>
+<Head title="Andorra is considering charging foreign cars that wish to enter the country" />
+<HomeNavbar></HomeNavbar>
+<!-- Blog Article -->
+<div class="max-w-2xl px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto">
+  <div class="max-w-2xl place-content-center">
+    <!-- Avatar Media -->
+    <div class="flex justify-between items-center mb-6">
+      <div class="flex w-full sm:items-center gap-x-5 sm:gap-x-3">
+        <div class="flex-shrink-0 p-6">
+          <img class="w-7 rounded-full" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Anna Ludic">
+        </div>
+
+        <div class="grow">
+          <div class="flex justify-between items-center gap-x-2">
+            <div>
+              <!-- Tooltip -->
+              <div class="hs-tooltip [--trigger:hover] [--placement:bottom] inline-block">
+                <div class="hs-tooltip-toggle sm:mb-1 block text-start cursor-pointer">
+                  <span class="font-semibold text-gray-800 dark:text-neutral-200">
+                    Anna Ludic
+                  </span>
+
+                  <!-- Dropdown Card -->
+                  <div class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 max-w-xs cursor-default bg-gray-900 divide-y divide-gray-700 shadow-lg rounded-xl dark:bg-neutral-950 dark:divide-neutral-700" role="tooltip">
+                  </div>
+                  <!-- End Dropdown Card -->
+                </div>
+              </div>
+              <!-- End Tooltip -->
+
+              <ul class="text-xs text-gray-500 dark:text-neutral-500">
+                <li class="inline-block relative pe-6 last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-2 before:-translate-y-1/2 before:size-1 before:bg-gray-300 before:rounded-full dark:text-neutral-400 dark:before:bg-neutral-600">
+                  19/08/2024
+                </li>
+                <li class="inline-block relative pe-6 last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-2 before:-translate-y-1/2 before:size-1 before:bg-gray-300 before:rounded-full dark:text-neutral-400 dark:before:bg-neutral-600">
+                  5 min read
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Avatar Media -->
+
+    <!-- Content -->
+    <div class="space-y-5 md:space-y-8">
+      <div class="space-y-3">
+        <h2 class="text-2xl font-bold md:text-3xl dark:text-white">Andorra is considering charging foreign cars that wish to enter the country</h2>
+
+        <p class="text-lg text-gray-800 dark:text-neutral-200">
+            The country is proposing a bill to address the massive influx of tourists by implementing a fee, in the form of a vignette, for foreign vehicles driving within its borders. The aim of this new measure is to curb the overwhelming tourism, after nearly 10 million visitors were recorded in 2023.</p>
+      </div>
+
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">The Andorran government has stated that the project needs to be agreed upon with the tourism and commercial sectors, but according to the <a href="https://www.segre.com/es/comarcas/240817/andorra-plantea-cobrar-a-todos-los-coches-extranjeros-que-circulen-por-sus-carreteras_536755.html" target="_blank"><b>Segre newspaper</b></a>, initial information from the Andorran executive indicates that the tax would have to be paid by “all foreign vehicles.”</p>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">This measure could force cross-border workers who commute daily to abandon the border, as well as those who travel for medical reasons, such as diagnostic tests. Regarding this issue, government sources have told the cited newspaper that cross-border workers “is an element that still needs to be worked on and developed,” and until the project progresses, it cannot be resolved. The new regulation would affect more than 1,500 residents of Alt Urgell who commute to Andorra daily for work.</p>
+      
+      <figure class="mt-6 mb-6">
+        <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1499933567928-f3385659fc54?q=80&w=2743&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Agreement between andorra and the EU">
+        <figcaption class="mt-3 text-sm text-center text-gray-500 dark:text-neutral-500">
+          “We need to reflect on whether it is an appropriate measure considering that we are a tourist country, but we want to aim for more qualitative tourism.” - Xavier Espot
+        </figcaption>
+      </figure>
+
+      <h3 class="mt-6 text-2xl font-semibold dark:text-white">Agile and Practical System</h3>
+      <ul>
+      <li class="mt-6 text-lg text-gray-800 dark:text-neutral-200">The head of the Andorran government, Xavier Espot, has detailed that a participatory process will be carried out among the country’s social and economic agents to study the incorporation of a vignette. In an interview with Catalunya Ràdio, he mentioned that they have given themselves time for reflection until at least the end of the year.</li>
+      <li class="mt-6 text-lg text-gray-800 dark:text-neutral-200">“We need to reflect on whether it is an appropriate measure considering that we are a tourist country, but we want to aim for more qualitative tourism.” However, he pointed out that it is “by no means a firm decision.</li>
+      <li class="mt-6 text-lg text-gray-800 dark:text-neutral-200">Regarding the possibility of establishing a vignette for tourists, he indicated that it would be “a reasonable amount,” since the Andorran economy is based on services and tourism. In this sense, he stated that “we are delighted that people visit our home, but we must aim for sustainable growth.”</li>
+      </ul>
+      
+      <h3 class="mt-6 text-2xl font-semibold dark:text-white">Tourism Overload in Andorra</h3>
+      <p class="text-lg text-gray-800 dark:text-neutral-200">The Andorran government considers that the country is being stretched to its capacity limit to ensure sustainability and that natural resources are sufficient.</p>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">Therefore, the measure being studied would mean that the cost of the vignette, which would be charged for the use of the road network in the country, as other European countries already do, would vary depending on whether it is high or low season.</p>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">Additionally, the length of the visitor’s stay in Andorra would also be taken into account. The fee in other countries is for a week, a month, or even a year for unlimited road use during this period.</p>
+
+      <h3 class="mt-6 text-2xl font-semibold dark:text-white">Over 9 Million Visitors</h3>
+      <p class="text-lg text-gray-800 dark:text-neutral-200">Tourist arrivals in Andorra continue to rise, with 9,367,636 visitors recorded in 2023, an 11.2% increase compared to the previous year. Most visitors to the country are between 45 and 64 years old, followed by those over 65, with the most common profile being couples and families.</p>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">The main reason for visitors is shopping, specifically 57.9%. Both day-trippers and tourists cite shopping as their primary reason for visiting, with 83.9% and 33.1%, respectively. <b>The number of vehicles entering Andorra in 2023 was 4,229,314.</b></p>
+
+      <div class="space-y-3">
+        <a href="https://www.elnacional.cat/es/sociedad/andorra-estudia-cobrar-coches-extranjeros-quieran-entrar-pais_1269894_102.html" target="_blank"><p class="mt-6 text-lg text-gray-800 dark:text-neutral-200 underline decoration-2">Check the original article in here</p></a>
+      </div>
+    </div>
+    <!-- End Content -->
+  </div>
+</div>
+<!-- End Blog Article -->
+<Footer></Footer>
+</template>
