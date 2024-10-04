2a3e3a4d630d69610e18beb0c39102b1eb65162c
Marc Juanmartí <marc@nectar.tech>
2024-10-04 09:49:21 +0200

    Adds new article

---
+++ resources/js/Pages/Articles/Andorra-hosts-freeride-world-championship-2026.vue
@@ -0,0 +1,95 @@
+<script setup>
+import { Head } from '@inertiajs/vue3';
+import HomeNavbar from '../../Components/HomeNavbar.vue';
+import Footer from '../../Components/Footer.vue';
+
+document.title = "Andorra to Host the First Ever Freeride World Championships in 2026 - Andorra Connect"
+</script>
+
+<template>
+<Head title="AR+I Sees a Positive Impact of the Agreement Between Andorra and the EU on Economic Growth" />
+<HomeNavbar></HomeNavbar>
+<!-- Blog Article -->
+<div class="max-w-2xl px-4 pt-6 lg:pt-10 pb-12 sm:px-6 lg:px-8 mx-auto">
+  <div class="max-w-2xl place-content-center">
+    <!-- Avatar Media -->
+    <div class="flex justify-between items-center mb-6">
+      <div class="flex w-full sm:items-center gap-x-5 sm:gap-x-3">
+        <div class="flex-shrink-0 p-6">
+          <img class="w-7 rounded-full" src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=facearea&facepad=2&w=320&h=320&q=80" alt="Anna Ludic">
+        </div>
+
+        <div class="grow">
+          <div class="flex justify-between items-center gap-x-2">
+            <div>
+              <!-- Tooltip -->
+              <div class="hs-tooltip [--trigger:hover] [--placement:bottom] inline-block">
+                <div class="hs-tooltip-toggle sm:mb-1 block text-start cursor-pointer">
+                  <span class="font-semibold text-gray-800 dark:text-neutral-200">
+                    Anna Ludic
+                  </span>
+
+                  <!-- Dropdown Card -->
+                  <div class="hs-tooltip-content hs-tooltip-shown:opacity-100 hs-tooltip-shown:visible opacity-0 transition-opacity inline-block absolute invisible z-10 max-w-xs cursor-default bg-gray-900 divide-y divide-gray-700 shadow-lg rounded-xl dark:bg-neutral-950 dark:divide-neutral-700" role="tooltip">
+                  </div>
+                  <!-- End Dropdown Card -->
+                </div>
+              </div>
+              <!-- End Tooltip -->
+
+              <ul class="text-xs text-gray-500 dark:text-neutral-500">
+                <li class="inline-block relative pe-6 last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-2 before:-translate-y-1/2 before:size-1 before:bg-gray-300 before:rounded-full dark:text-neutral-400 dark:before:bg-neutral-600">
+                  04/10/2024
+                </li>
+                <li class="inline-block relative pe-6 last:pe-0 last-of-type:before:hidden before:absolute before:top-1/2 before:end-2 before:-translate-y-1/2 before:size-1 before:bg-gray-300 before:rounded-full dark:text-neutral-400 dark:before:bg-neutral-600">
+                  4 min read
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- End Avatar Media -->
+
+    <!-- Content -->
+    <div class="space-y-5 md:space-y-8">
+      <div class="space-y-3">
+        <h2 class="text-2xl font-bold md:text-3xl dark:text-white">Andorra to Host the First Ever Freeride World Championships in 2026</h2>
+
+        <p class="text-lg text-gray-800 dark:text-neutral-200">
+            The Freeride World Tour (FWT) has announced that Andorra will be the venue for the first-ever FIS Freeride World Championships, scheduled to take place from <b>February 1 to 6, 2026</b>, at the Ordino Arcalís resort.</p>
+      </div>
+
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">This event will mark a historic moment for the sport, as it will be the first time that freeride will hold World Championships under the umbrella of the International Ski Federation (FIS).</p>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">For over a decade, Andorra has been a premier destination for freeride, and hosting the World Championships will solidify its position as a global epicenter for this discipline.</p>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">The event will not only bring together the best freeriders from around the world but also elevate the sport to new heights, providing an unprecedented platform for freeride skiing and snowboarding.</p>
+      
+      <figure class="mt-6 mb-6">
+        <img class="w-full object-cover rounded-xl" src="https://images.unsplash.com/photo-1512334624157-6f47e948814c?q=80&w=2074&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" alt="Andorra to Host the First Ever Freeride World Championships in 2026">
+        <figcaption class="mt-3 text-sm text-center text-gray-500 dark:text-neutral-500">
+          Andorra has proven to be a premium freeride destination for over a decade, with first-class settings and exceptional hospitality.
+        </figcaption>
+      </figure>
+
+      <h3 class="mt-6 text-2xl font-semibold dark:text-white">The First FIS Freeride World Tour in Andorra</h3>
+      <ul>
+      <li class="mt-6 text-lg text-gray-800 dark:text-neutral-200">David Ledesma, the marketing director of Grandvalira Resorts, explained, “After more than 10 years of a permanent presence in the FWT circuit, we believed our commitment to freeride should reach new goals. We worked with FIS Freeride to become the first organizers in the history of the World Championships in our sport. For Andorra and Ordino Arcalís, which have already become a global reference in freeride, it is important to create a legacy that helps freeride grow.”</li>
+      <li class="mt-6 text-lg text-gray-800 dark:text-neutral-200">Nicolas Hale-Woods, the founder and CEO of the Freeride World Tour, emphasized the Principality’s excellence as a host, highlighting the destination’s premium status: “Andorra has proven to be a premium freeride destination for over a decade, with first-class settings and exceptional hospitality. FIS Freeride is very excited to make history with our Andorran partners. The FIS Freeride World Championships are an important step in bringing freeride skiing and snowboarding to audiences worldwide.”</li>
+      <li class="mt-6 text-lg text-gray-800 dark:text-neutral-200">The qualification system for these inaugural FIS Freeride World Championships will be announced in December 2024, following final validation by the FIS Council on October 23.</li>
+      </ul>
+      
+      <h3 class="mt-6 text-2xl font-semibold dark:text-white">FIS Freeride World Tour by Peak Performance 2025 Schedule</h3>
+      <p class="text-lg text-gray-800 dark:text-neutral-200">Additionally, the schedule for the FIS Freeride World Tour by Peak Performance 2025 has been announced, featuring six events worldwide.</p>
+      <p class="mt-6 text-lg text-gray-800 dark:text-neutral-200">This time, Andorra will not be one of the 2025 circuit venues, as it will be fully focused on organizing the 2026 World Championships, reaffirming its commitment to the success of this historic event.</p>
+
+      <div class="space-y-3">
+        <a href="https://www.marca.com/deportes-invierno/2024/10/03/andorra-celebrara-primeros-campeonatos-mundo-freeride-historia-2026.html" target="_blank"><p class="mt-6 text-lg text-gray-800 dark:text-neutral-200 underline decoration-2">Check the original article in here</p></a>
+      </div>
+    </div>
+    <!-- End Content -->
+  </div>
+</div>
+<!-- End Blog Article -->
+<Footer></Footer>
+</template>
